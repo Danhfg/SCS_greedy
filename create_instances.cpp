558ff6199c732aa4dcfa4f8c9346b73da35d8231
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2019-08-19 21:01:50 -0300

    modify comments

--- create_instances.cpp
+++ create_instances.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist50(1,50); // distribution in range [1, 6]
+    std::uniform_int_distribution<std::mt19937::result_type> dist50(1,50); 
     std::uniform_int_distribution<std::mt19937::result_type> dist4(0,3);
     std::vector<std::string> alphab = {"A","C","T","G"};
     int i = 0;
